8668a224af246b0fbd8c276753d2a27a12582a43
gonnagetbetter <denissss906@gmail.com>
2025-08-29 16:01:32 +0300

    Integrate Swagger for API documentation and update AppModule with necessary modules.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { config } from './config'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { cleanupOpenApiDoc } from 'nestjs-zod'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  await app.listen(3000)
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Nest Test Task API')
+    .setDescription('API documentation for Nest Test Task')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+
+  const rawDocument = SwaggerModule.createDocument(app, swaggerConfig)
+
+  const document = cleanupOpenApiDoc(rawDocument)
+
+  SwaggerModule.setup('api/docs', app, document)
+
+  await app.listen(config.port)
 }
-bootstrap()
+
+bootstrap().catch(err => {
+  console.error('Error during bootstrap:', err)
+  process.exit(1)
+})
